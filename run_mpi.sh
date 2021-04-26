684d560ff11b99eb4648be212b9c6383faccbf75
Leonardo Gurgel <lgurgel@id.uff.br>
2021-04-25 22:09:55 -0300

    add scripts

--- run_mpi.sh
+++ run_mpi.sh
@@ -1,14 +1,11 @@
-#!/bin/zsh
+#!/bin/bash
 
 for n in 4 10 20 50 100 200 500 1000 2000; do
-  echo "--- n = $n ---\nsingle\n" >> test.log
-  for i in 1 2 3; do
-    ./single.out $n "benchmark/matrix_$n" >> test.log
-  done
+  echo "--- n = $n ---" >> test.log
   for p in 2 4 6 8; do
-    echo "\np=$p" >> test.log
+    echo "p=$p" >> test.log
     for i in 1 2 3; do
-      mpirun --oversubscribe -n $p ./main.out $n "benchmark/matrix_$n" >> test.log
+      mpirun --oversubscribe -n $p main.out $n "benchmark/matrix_$n" >> test.log
     done
   done
 done
